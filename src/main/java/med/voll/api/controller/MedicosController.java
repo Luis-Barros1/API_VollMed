09f1d1b51e206f965510adac66f4185527a13f9d
Luis Barros <luisrochabarros@hotmail.com>
2023-03-22 16:45:50 -0300

    Primeiro Commit

---
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicosController {
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarMedico(@RequestBody DadosCadastroMedico dados){
+        medicoRepository.save(new Medico(dados));
+    }
+}
