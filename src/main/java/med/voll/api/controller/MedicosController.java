6e8282c5c8d87781cedefd038e9d86ddc81e72f7
Luis Barros <luisrochabarros@hotmail.com>
2023-03-25 16:18:24 -0300

    Adicionada listagem de médicos com paginação

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -3,11 +3,17 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -20,5 +26,10 @@ public class MedicosController {
     @ResponseStatus(HttpStatus.CREATED)
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados){
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
