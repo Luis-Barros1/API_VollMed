150f3e40bfee55cab33ccdbf24313ebbcf2b746d
Luis Barros <luisrochabarros@hotmail.com>
2023-03-27 13:31:51 -0300

    Organização de pacotes e tratamento de erros de requisição

--- src/main/java/med/voll/api/domain/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosListagemPaciente.java
@@ -1,7 +1,4 @@
-package med.voll.api.paciente;
-
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
+package med.voll.api.domain.paciente;
 
 public record DadosListagemPaciente(long id, String nome, String email, String cpf) {
 
